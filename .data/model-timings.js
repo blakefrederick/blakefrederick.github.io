81e6323a53fa3d6e7a4292da72d7cc021856bf6d
Blake Frederick <bf@six12creative.com>
2022-03-07 12:07:57 -0800

    ok

---
+++ .data/model-timings.js
@@ -0,0 +1,14 @@
+const ModelTimings = {
+    1: "https://raw.githubusercontent.com/alteous/glTF-Sample-Models-Official/master/2.0/SciFiHelmet/glTF/SciFiHelmet.gltf",
+    2: "https://raw.githubusercontent.com/alteous/glTF-Sample-Models-Official/master/2.0/SmilingFace/glTF/SmilingFace.gltf",
+    3: "https://raw.githubusercontent.com/alteous/glTF-Sample-Models-Official/master/2.0/AnimatedMorphSphere/glTF/AnimatedMorphSphere.gltf",
+    4: "https://github.com/alteous/glTF-Sample-Models-Official/blob/master/2.0/Avocado/glTF/Avocado.gltf",
+    5: "https://raw.githubusercontent.com/alteous/glTF-Sample-Models-Official/master/2.0/BarramundiFish/glTF/BarramundiFish.gltf",
+    6: "https://raw.githubusercontent.com/blakefrederick/blakefrederick.github.io/master/ar-scan/models/animated-treasure/ar_treasure_chest_0513b.gltf",
+    7: "https://raw.githubusercontent.com/blakefrederick/blakefrederick.github.io/master/ar-scan/models/SPK_AR_Demo_0125c.glb",
+    8: "https://raw.githubusercontent.com/blakefrederick/blakefrederick.github.io/master/ar-scan/models/SPK_AR_Demo_0125c.glb",
+    9: "https://raw.githubusercontent.com/blakefrederick/blakefrederick.github.io/master/ar-scan/models/SPK_AR_Demo_0125c.glb",
+    0: "https://raw.githubusercontent.com/blakefrederick/blakefrederick.github.io/master/ar-scan/models/SPK_AR_Demo_0125c.glb",
+}
+
+export default ModelTimings
